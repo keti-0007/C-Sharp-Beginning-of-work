d58f6c3a92137c6b246ea0330e9fddd85b92e200
Ekaterina <makrusova96@mail.ru>
2023-01-17 16:45:39 +0300

    Задача №2

---
+++ Hw_1_tasc_1/Program.cs
@@ -0,0 +1,15 @@
+﻿Console.Clear();
+Console.Write("Введите первое число: ");
+int a = Convert.ToInt32(Console.ReadLine());
+
+Console.Write("Введите второе число: ");
+int b = Convert.ToInt32(Console.ReadLine());
+
+if ( a > b )
+{
+    System.Console.Write($"max {a}, min {b}");
+}
+if (b > a )
+{
+     System.Console.Write($"max {b}, min {a}");
+}
